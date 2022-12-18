4e2ec13acf0962e6876a7783548fd51dee075c07
OSi (Ondrej Sienczak) <ondrej.sienczak@seznam.cz>
2022-12-18 13:04:43 +0100

    Fixed time shift

--- simulator/ltime.py
+++ simulator/ltime.py
@@ -1,14 +1,13 @@
+from db import time
+from utime import localtime
 from ulogging import getLogger
 logger = getLogger(__name__)
-
-from utime import localtime
-from db import time
 
 
 class Time():
     def __init__(self, timezone):
         wt_offset = 3600 * time.WINTER
-        self.time_offset = -946670400 - wt_offset - timezone
+        self.time_offset = -946674000 - wt_offset - timezone
 
     def get_date_time(self, unix_time):
         return localtime(unix_time + self.time_offset)
